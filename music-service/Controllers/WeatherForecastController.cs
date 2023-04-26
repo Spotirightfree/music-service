2877206286aa72adfb5a4dccc16d8dd32426cc0e
BillyKuiper <456181@student.fontys.nl>
2023-04-26 16:47:52 +0200

    add docker config

--- music-service/Controllers/WeatherForecastController.cs
+++ music-service/Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using System.Text;
 
 namespace music_service.Controllers
 {
@@ -28,6 +30,63 @@ namespace music_service.Controllers
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+
+        [HttpPost]
+        [Route("SendMessagePlaylist")]
+        public void sendMessage()
+        {
+            var factory = new ConnectionFactory { HostName = "192.168.240.6" };
+            factory.UserName = "playlist-service";
+            factory.Password = "playlist-service";
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "playlist-service-queue",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+            const string message = "Hello World!";
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: string.Empty,
+                                 routingKey: "playlist-service-queue",
+                                 basicProperties: null,
+                                 body: body);
+        }
+
+        [HttpPost]
+        [Route("SendMessageLogin")]
+        public void sendMessageLogin()
+        {
+            var factory = new ConnectionFactory { HostName = "192.168.240.6" };
+            factory.UserName = "playlist-service";
+            factory.Password = "playlist-service";
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare(queue: "login-service-queue",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+            const string message = "Hello World!";
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: string.Empty,
+                                 routingKey: "login-service-queue",
+                                 basicProperties: null,
+                                 body: body);
+        }
+
+        [HttpGet]
+        [Route("Hello_World")]
+        public string HelloWorld()
+        {
+            return "Hello World from Music-Service";
         }
     }
 }
--- music-service/Program.cs
+++ music-service/Program.cs
@@ -1,14 +1,21 @@
+using Microsoft.AspNetCore.Connections;
+using music_service.Controllers;
+using music_service.Services;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Runtime.CompilerServices;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+builder.Services.AddHostedService<RabbitMQBackgroundWorkerService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -16,10 +23,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
